d5d64013dd6b68fc51b59741060f16fe5ae36d6d
Satyajeet K. Thakare <satyajeetthakre@gmail.com>
2016-04-21 14:53:23 +0530

    * Dashboard suggested UI changes. * Rate master UI changes.

--- src/app/ratequery/ratequery.controller.js
+++ src/app/ratequery/ratequery.controller.js
@@ -2,51 +2,30 @@ export class RateQueryController {
   //constructor ($timeout, webDevTec, toastr) {
   constructor($timeout, apiService) {
     'ngInject';
-    //this.$http = $http;
     this._api = apiService;
-    //this.toaster = toaster;
-     this.getAllFuelRates();
-     this.getAllCity();
+    this.getAllFuelRates();
+    this.getAllCity();
   }
-
-/*//Test
-
-  getTransactionData(isFlag, fromLoc, vehTypeName)
-  {
-
-    this._api.get(`location/0`)
-         .then((res) => {
-          this.outputdata = res.data;
-          var newOutputData =
-              _.each(this.outputdata,
-                     (key, value) => {
-}
-}
-//Test*/
 
   init() {
 
-/* this.city_option = _.map([LOCATIONNAME], (i) => ({
-      name: i,
-      val: i
-    }));*/
-
   }
-     getAllFuelRates(){
-       this._api.get('getrates/all').then((res) => {
-        this.fuelRates = res.data;
-        console.log(this.fuelRates);
-      }, (err) => {
-        this.toaster.error(`${err.status} : ${err.statusText}`);
-      });
-    }
 
-    getAllCity(){
-     this._api.get('location/0').then((res) => {
-        this.city = res.data;
-       console.log(this.city);
-      }, (err) => {
-        this.toaster.error(`${err.status} : ${err.statusText}`);
-      });
-   }
+  getAllFuelRates() {
+    this._api.get('getrates/all').then((res) => {
+      this.fuelRates = res.data;
+      console.log(this.fuelRates);
+    }, (err) => {
+      this.toaster.error(`${err.status} : ${err.statusText}`);
+    });
+  }
+
+  getAllCity() {
+    this._api.get('location/0').then((res) => {
+      this.city = res.data;
+      console.log(this.city);
+    }, (err) => {
+      this.toaster.error(`${err.status} : ${err.statusText}`);
+    });
+  }
 }
