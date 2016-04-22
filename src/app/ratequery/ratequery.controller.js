eac099545611c35b49766ff12d2ea2c8d10235fd
praveensinghhart <23148127@mahindra.com>
2016-04-22 10:20:50 +0530

    * Change URL * Filter option change

--- src/app/ratequery/ratequery.controller.js
+++ src/app/ratequery/ratequery.controller.js
@@ -6,49 +6,55 @@ export class RateQueryController {
     this.$stateParams = $stateParams;
     this.$filter = $filter;
 
-
     this.getAllCity();
 
     this.FUELTODATE = new Date();
-
     var fuelFromDate = new Date();
     fuelFromDate.setDate(fuelFromDate.getDate() - 180);
     this.FUELFROMDATE = fuelFromDate;
 
-    this.getAllFuelRates();
+    this.getAllFuelRates(this.$stateParams.cityid);
   }
 
   init() {
 
   }
 
-  getAllFuelRates() {
-
-    var fromdt =this.$filter('date')(new Date(this.FUELFROMDATE), 'yyyy-MM-dd');
-    var todt =this.$filter('date')(new Date(this.FUELTODATE), 'yyyy-MM-dd');
-
-    var urlnew = `getratequery/${this.$stateParams.cityid}/${fromdt}/${todt}`;
-    console.log(urlnew);
-
-    this._api.get(`getratequery/${this.$stateParams.cityid}/${fromdt}/${todt}`).then((res) => {
-      this.fuelRates = res.data;
-    }, (err) => {
-      this.toaster.error(`${err.status} : ${err.statusText}`);
-    });
-  }
-
   getAllCity() {
     this._api.get('location/0').then((res) => {
       this.city = res.data;
-      //console.log(this.city);
 
       var getid = this.$stateParams.cityid;
       var cityid = parseInt(getid);
-      console.log(cityid);
       this.selectedOption = _.find(this.city, ['LOCATIONID', cityid]);
-      console.log(this.selectedOption);
     }, (err) => {
       this.toaster.error(`${err.status} : ${err.statusText}`);
     });
   }
+
+  getAllFuelRates(cityid) {
+
+    var fromdt = this.$filter('date')(new Date(this.FUELFROMDATE), 'yyyy-MM-dd');
+    var todt = this.$filter('date')(new Date(this.FUELTODATE), 'yyyy-MM-dd');
+    var urlnew = `getratequery/${cityid}/${fromdt}/${todt}`;
+
+    this._api.get(`getratequery/${cityid}/${fromdt}/${todt}`).then((res) => {
+      this.fuelRates = res.data;
+
+      var newOutputData =
+        _.each(this.fuelRates,
+          (key, value) => {
+            var FUELDATE = this.fuelRates[value].FUELDATE;
+            FUELDATE = this.$filter('date')(new Date(this.fuelRates[value].FUELDATE), 'dd-MMM-yyyy');
+            this.fuelRates[value].FUELDATE = FUELDATE;
+          });
+
+        this.fuelRates = newOutputData;
+
+    }, (err) => {
+      this.toaster.error(`${err.status} : ${err.statusText}`);
+    });
+  }
+
+
 }
